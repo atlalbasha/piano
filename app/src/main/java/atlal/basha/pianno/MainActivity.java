479aa8a9169b1eee5d18ead06c364f7e5c028b99
atlalbasha <atlal.basha@gmail.com>
2020-11-18 14:57:49 +0100

    piano working well

--- app/src/main/java/atlal/basha/pianno/MainActivity.java
+++ app/src/main/java/atlal/basha/pianno/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
@@ -19,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer ;
 
-    private int sound = R.raw.piano;
+    ArrayList<Integer> soundList = new ArrayList<>();
+    private int sound1 = R.raw.piano1;
+    private int sound2 = R.raw.piano2;
+    private int sound3 = R.raw.piano3;
+    private int sound4 = R.raw.piano4;
+    private int sound5 = R.raw.piano5;
+    private int sound6 = R.raw.piano6;
+    private int sound7 = R.raw.piano7;
 
 
     @Override
@@ -35,13 +45,67 @@ public class MainActivity extends AppCompatActivity {
         button6 = findViewById(R.id.button5);
         button7 = findViewById(R.id.button6);
 
+        soundList.add(sound1);
+        soundList.add(sound2);
+        soundList.add(sound3);
+        soundList.add(sound4);
+        soundList.add(sound5);
+        soundList.add(sound6);
+        soundList.add(sound7);
+
+        //mediaPlayer = MediaPlayer.create(this, soundList.get(0));
+        sound(0);
 
 
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.piano);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mediaPlayer.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(1);
+                mediaPlayer.start();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(2);
+                mediaPlayer.start();
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(3);
+                mediaPlayer.start();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(4);
+                mediaPlayer.start();
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(5);
+                mediaPlayer.start();
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound(6);
                 mediaPlayer.start();
             }
         });
@@ -58,5 +122,10 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         mediaPlayer.release();
         mediaPlayer = null;
+    }
+
+    public int sound (int number){
+        mediaPlayer = MediaPlayer.create(this, soundList.get(number));
+        return sound1;
     }
 }
